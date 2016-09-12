17cc24fbd454895ff939792b991c6d82279300c6
LefkaLefka <lefka.velchenko@gmail.com>
2016-09-12 12:45:25 +0300

    refractoring, add function clear completed tasks

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-// создаем массив записей
-// ссотоит из объктов вида
-// record = {statusRecord: false, textRecord: ""}
-var arrayRecord = new Array();
+// создаем массив задач
+// состоит из объктов вида
+// task = {statusTask: false, textTask: ""}
+var arrayTasks = new Array();
 
-//назначаем функций обработчика событий
+// назначаем функций обработчика событий
 window.onload = function()
 {
-	document.getElementById("inputFormNewRecord").addEventListener("keypress", AddNewRecordToArray);
-//	document.getElementById("checkboxExpandTurnRecords").addEventListener("click", ExpandTurnRecords);
+	document.getElementById("inputFormNewTask").addEventListener("keypress", AddNewTaskToArray);
+//	document.getElementById("checkboxExpandTurnTasks").addEventListener("click", ExpandTurnRecords);
 }
 
-// добавляем новое значение в массив записей
-function AddNewRecordToArray()
+// добавляем новую задачу в массив задач
+function AddNewTaskToArray()
 {
 	var e = e || window.event;
 	// добавляем по событию нажатия кнопки "Enter"
 	if (e.keyCode === 13)
 	{
 		// проверяем входящу строку
-		var str = document.getElementById("inputFormNewRecord").value;
+		var str = document.getElementById("inputFormNewTask").value;
 		if (str !== "")
 		{
 			// создаем объект и добаляем его в массив
-			arrayRecord[arrayRecord.length] = {statusRecord: 0, textRecord: str};
+			arrayTasks[arrayTasks.length] = {statusTask: 0, textTask: str};
 			// обновляем входищий "input"
-			document.getElementById("inputFormNewRecord").value = "";
+			document.getElementById("inputFormNewTask").value = "";
 			// создаем и добавляем эелемент на форму
-			ModelingPasteStrLiElement(arrayRecord.length - 1);
+			ModelingPasteStrLiElement(arrayTasks.length - 1);
+			// обновляем запись о количестве оставшихся задач
+			UpdateNumberUnfinishedTasks();
 		}
 	}
 }
 
 // формируем строку которая будет содеражть все внутренные элементы <li>
-// принимает порядковый номер записи в массиве для id
+// принимает порядковый номер задачи в массиве для id
 function ModelingPasteStrLiElement(num)
 {
 	var newList = document.createElement("li");
@@ -53,14 +55,14 @@ function ModelingStrCheckboxDone(num)
 // формируем html-теги для div-а с текстом записи
 function ModelingStrDivText(num)
 {
-	var str = "<div class=\"text\" " + "id=" + num + ".1>" + arrayRecord[num].textRecord + "</div>";
+	var str = "<div class=\"text\" " + "id=" + num + ".1>" + arrayTasks[num].textTask + "</div>";
 	return str;
 }
 
 // формируем html-теги для div-а с кнопкой удаления записи
 function ModelingStrButtonDeleteDivSpan(num)
 {
-	var str = "<div class=\"buttonDelete\" " + "id=" + num + ".2 " + "onselectstart=\"return false\" onmousedown=\"return false\"><span class=\"buttonDeleteRecord\"" + "id=" + num + ".2.0"  + ">X</span></div>";
+	var str = "<div class=\"buttonDelete\" " + "id=" + num + ".2 " + "onselectstart=\"return false\" onmousedown=\"return false\"><span class=\"buttonDeleteTask\"" + "id=" + num + ".2.0"  + ">X</span></div>";
 	return str;
 }
 
@@ -71,68 +73,108 @@ document.onclick = function(e)
 	var target = e.target || e.srcElement;
 	
 	// клик на кнопку удаления записи
-	if (target.className === "buttonDeleteRecord")
+	if (target.className === "buttonDeleteTask")
 	{
 		// id элемента который удаляем
 		var id = target.id.replace(/.2.0/, "");
 		var idInt = parseInt(id);
 		// удаляем элемент из списка ul
-		DeleteRecordFromUl(id);
+		DeleteTaskFromUl(id);
 		// удаляем элемент из массива записей
-		DeleteRecordFromArray(idInt);
+		DeleteTaskFromArray(idInt);
 		// обновляем id в списке ul
 		UpdateDataListId(idInt);
+		// обновляем запись о количестве оставшихся задач
+		UpdateNumberUnfinishedTasks();
 	}
-	// клик на checkbox - статус задания
+	// клик на checkbox - статус задачи
 	if (target.className === "checkboxDone")
 	{
 		// id элемента которому меняем статус
 		var id = target.id.replace(/.0/, "");
 		var idInt = parseInt(id);
-		
-		console.log(arrayRecord[idInt].statusRecord);
-		arrayRecord[idInt].statusRecord ^= true;
-		console.log(arrayRecord[idInt].statusRecord);
+		// меняем статус
+		arrayTasks[idInt].statusTask ^= true;
+		// обновляем запись о количестве оставшихся задач
+		UpdateNumberUnfinishedTasks();
 	}
-	
-//	console.log(arrayRecord);
+	if (target.id === "clearCompleted")
+	{
+		ClearCompleted();
+	}	
 }
 
 // удаление записи
-// удаление всех элементов внутри тега <li> включая его самого с id===num
-function DeleteRecordFromUl(id)
+// удаление всех элементов внутри тега <li> включая его самого с заданым id
+function DeleteTaskFromUl(id)
 {
 	var element = document.getElementById(id);
 	element.remove();
 }
 
 // удаляем запись из массива
-function DeleteRecordFromArray(index)
+function DeleteTaskFromArray(index)
 {
 	// проходим от элемента который надо удалить до конца списка переприсваивая объекты
-	for (var i = index; i < arrayRecord.length - 1; i++)
+	for (var i = index; i < arrayTasks.length - 1; ++i)
 	{
-		arrayRecord[i].statusRecord = arrayRecord[i + 1].statusRecord;
-		arrayRecord[i].textRecord = arrayRecord[i + 1].textRecord;
+		arrayTasks[i].statusTask = arrayTasks[i + 1].statusTask;
+		arrayTasks[i].textTask = arrayTasks[i + 1].textTask;
 	}
 	// удаляем последний элемент
-	arrayRecord.splice(arrayRecord.length - 1, 1);
+	arrayTasks.splice(arrayTasks.length - 1, 1);
 }
 
-// обновление списка всех записей на экране
+// обновление списка всех задач на экране
 function UpdateDataListId(id)
 {
-	for (var i = id + 1; i < arrayRecord.length + 1; ++i)
+	for (var i = id + 1; i < arrayTasks.length + 1; ++i)
 	{
 		// обновляем id элемента li
 		document.getElementById(i.toString()).id = (i - 1).toString();
-		// обновляем id элемента checkbox(статус задания)
+		// обновляем id элемента checkbox(статус задачи)
 		document.getElementById(i.toString() + ".0").id = (i - 1).toString() + ".0";
-		// обновляем id элемента div(текст задания)
+		// обновляем id элемента div(текст задачи)
 		document.getElementById(i.toString() + ".1").id = (i - 1).toString() + ".1";
 		// обновляем id элемента div(кнопка удаления)
 		document.getElementById(i.toString() + ".2").id = (i - 1).toString() + ".2";
 		// обновляем id элемента span(кнопка удаления)
 		document.getElementById(i.toString() + ".2.0").id = (i - 1).toString() + ".2.0";
+	}
+}
+
+// обновление записи о количестве оставшихся задач
+function UpdateNumberUnfinishedTasks()
+{
+	var number = 0;
+	// подсчитываем оставшиеся задачи
+	for (var i = 0; i < arrayTasks.length; ++i)
+	{
+		if (!arrayTasks[i].statusTask)
+			++number;
+	}
+	// отображаем их пользователю
+	if (number > 1)
+		document.getElementById("unfinishedTasks").innerHTML = number + " toDos left";
+	else
+		document.getElementById("unfinishedTasks").innerHTML = number + " toDo left";
+}
+
+// удаление завершенных задач
+function ClearCompleted()
+{
+	for (var i = 0; i < arrayTasks.length; ++i)
+	{
+		// проверяем статус задачи
+		if (arrayTasks[i].statusTask)
+		{
+			// удаляем элемент из списка ul
+			DeleteTaskFromUl(i.toString());
+			// удаляем элемент из массива записей
+			DeleteTaskFromArray(i);
+			// обновляем id в списке ul
+			UpdateDataListId(i);
+			--i;
+		}
 	}
 }
